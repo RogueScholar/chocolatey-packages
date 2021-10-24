3634cab829a3cf2920346d2f59764579991cdb20
Peter J. Mello <admin@petermello.net>
2021-10-24 09:00:02 -0700

    Update hledger packaging to new release v1.23
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- automatic/hledger/tools/chocolateyinstall.ps1
+++ automatic/hledger/tools/chocolateyinstall.ps1
@@ -1,12 +1,17 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/simonmichael/hledger/releases/download/1.22.2/hledger-windows.zip'
+﻿# SPDX-FileCopyrightText: © 2020-2021 Nicholas Smith <nsmith@ethosgroup.com>
+# SPDX-FileCopyrightText:  2021 Peter J. Mello <admin@petermello.net>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = 'https://github.com/simonmichael/hledger/releases/download/1.23/hledger-windows.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = 'd7ed8b4ac563e6f9d6badbd1eb9d9cf2da873d6685628710c1cd8c472d42edaf'
+  checksum      = '51d3fc0af43454f7f2c095473474d66905fc4d8d6e420a4aa0c81b58833728eb'
   checksumType  = 'sha256'
 }
 
--- automatic/hledger/update.ps1
+++ automatic/hledger/update.ps1
@@ -1,31 +1,36 @@
+# SPDX-FileCopyrightText: © 2020-2021 Nicholas Smith <nsmith@ethosgroup.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 Import-Module au
 
 $releases = 'https://github.com/simonmichael/hledger/releases'
 
 function global:au_BeforeUpdate {
-    $Latest.Checksum32 = Get-RemoteChecksum -Algorithm sha256 $Latest.URL
+  $Latest.Checksum32 = Get-RemoteChecksum -Algorithm sha256 $Latest.URL
 }
 function global:au_SearchReplace {
-    @{
-        "tools\chocolateyinstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'" 
-        }
+  @{
+    "tools\chocolateyinstall.ps1" = @{
+      "(^[$]url\s*=\s*)('.*')"       = "`$1'$($Latest.URL)'"
+      "(checksum\s*=\s*)('.*')"      = "`$1'$($Latest.Checksum32)'" 
     }
+  }
 }
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    $re = 'hledger-windows.zip'
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $sourceUrl = 'https://github.com' + $url
-    $version = ($url -split '\/' | select -Index 5).Substring(0)
-
-    return @{
-        URL = $sourceUrl
-        Version = $version
-        ChecksumType32 = 'sha256'
-    }
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  
+  $re            = 'hledger-windows.zip'
+  $url           = $download_page.links | Where-Object href -match $re |
+                                          Select-Object -First 1 -expand href
+  $sourceUrl     = 'https://github.com' + $url
+  $version       = ($url -split '\/' | Select-Object -Index 5).Substring(0)
+  
+  return @{
+    URL            = $sourceUrl
+    Version        = $version
+    ChecksumType32 = 'sha256'
+  }
 }
 
 update -ChecksumFor none
