6766786651770aa0b9137282a6af1f4397371695
Peter J. Mello <admin@petermello.net>
2021-10-24 04:49:55 -0700

    Update Whalebird packaging to new release v4.4.5
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- automatic/whalebird/tools/chocolateyinstall.ps1
+++ automatic/whalebird/tools/chocolateyinstall.ps1
@@ -1,27 +1,32 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿# SPDX-FileCopyrightText: © 2020-2021 Nicholas Smith <nsmith@ethosgroup.com>
+# SPDX-FileCopyrightText:  2021 Peter J. Mello <admin@petermello.net>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+$ErrorActionPreference = 'Stop';
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'whalebird'
 
-$url32        = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.4.3/Whalebird-4.4.3-windows-ia32.exe'
-$url64      = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.4.3/Whalebird-4.4.3-windows-x64.exe'
-$checksum   = '922e54fa2fa3dffac395968ab407ae42beb7e37d753ec53821a396f78bda638bf20c26ceb718f582913d731d0ba8424362c3591cee5e1b902c3f47bb06b2904a'
-$checksum64  = '03d5629a17732d7199b971f753ca69e6b8d24ae9a28cbaf603f7dde5ee0266a251deef47deaebcf37db0880ba34903ed5d2974c85fac7e1d19d834867c6abd45'
+$url32       = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.4.5/Whalebird-4.4.5-windows-ia32.exe'
+$url64       = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.4.5/Whalebird-4.4.5-windows-x64.exe'
+$checksum    = 'dcc6483afbfa6ac6aaeb3e5e9bfc193d0b821b0c1367aa731285848d2f9a55656fefb66432b46c94ea32d01f872607ef70fb227da10eefc0f96d5aeb95558fca'
+$checksum64  = '7e7a611bf39757c972e30255b646adb4da15602fd01cb1435767ad2f0920678788158a5493f2212a1edfa7bd042b3a9e202e76cf952656ab45474246c67f9035'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url32
-  url64bit      = $url64
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url32
+  url64bit       = $url64
 
-  softwareName  = 'whalebird*'
-  checksum      = $checksum
-  checksumType32  = 'sha512'
-  checksum64    = $checksum64
-  checksumType64= 'sha512'
+  softwareName   = 'whalebird*'
+  checksum       = $checksum
+  checksumType32 = 'sha512'
+  checksum64     = $checksum64
+  checksumType64 = 'sha512'
 
-  silentArgs    = '/S'
-  validExitCodes= @(0)
+  silentArgs     = '/S'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
--- automatic/whalebird/update.ps1
+++ automatic/whalebird/update.ps1
@@ -1,39 +1,43 @@
+# SPDX-FileCopyrightText: © 2020-2021 Nicholas Smith <nsmith@ethosgroup.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 Import-Module au
 
 $releases = 'https://github.com/h3poteto/whalebird-desktop/releases'
 
 function global:au_BeforeUpdate() {
-    $Latest.Checksum32 = Get-RemoteChecksum -Algorithm sha512 $Latest.URL
-    $Latest.Checksum64 = Get-RemoteChecksum -Algorithm sha512 $Latest.URL64
+  $Latest.Checksum32 = Get-RemoteChecksum -Algorithm sha512 $Latest.URL
+  $Latest.Checksum64 = Get-RemoteChecksum -Algorithm sha512 $Latest.URL64
 }
 
 function global:au_SearchReplace {
-    Write-Host($Latest.Checksum32)
-    @{
-        "tools\chocolateyinstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(^[$]url64\s*=\s*)('.*')"  = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'" 
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
+  Write-Host($Latest.Checksum32)
+  @{
+    "tools\chocolateyinstall.ps1" = @{
+      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'" 
+      "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
+  }
 }
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    $re = "windows"
-    $url = $download_page.links | ? href -match $re | select -First 2 -expand href
-    $sourceUrl = 'https://github.com' + $url[0]
-    $sourceUrl64 = 'https://github.com' + $url[1]
-    $version = ($url[0] -split '\/' | select -Index 5).Substring(0)
-
-    return @{
-        URL = $sourceUrl
-        URL64 = $sourceUrl64
-        Version = $version
-        ChecksumType32 = 'sha512'
-        ChecksumType64 = 'sha512'
-    }
+  $download_page   = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $re              = "windows"
+  $url             = $download_page.links | Where-Object href -match $re |
+                       Select-Object -First 2 -expand href
+  $sourceUrl       = 'https://github.com' + $url[0]
+  $sourceUrl64     = 'https://github.com' + $url[1]
+  $version         = ($url[0] -split '\/' | Select-Object -Index 5).Substring(0)
+  
+  return @{
+    URL            = $sourceUrl
+    URL64          = $sourceUrl64
+    Version        = $version
+    ChecksumType32 = 'sha512'
+    ChecksumType64 = 'sha512'
+  }
 }
 
 update -ChecksumFor none
